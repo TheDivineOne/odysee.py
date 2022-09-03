ebbb7f764f9d25eeca3d65486799eb37cefb5c4c
Divine <111114451+TheDivineOne@users.noreply.github.com>
2022-09-03 11:57:36 +0200

    Update odysee.py

--- odysee.py
+++ odysee.py
@@ -1,4 +1,4 @@
 import requests,ast
 
 def getVideoMetadata(url,timeout=7):
-     return ast.literal_eval(requests.get(url,timeout=timeout).text.split('<script type="application/ld+json>')[1].split("</script>")[0])
+     return ast.literal_eval(requests.get(url,timeout=timeout).text.split('<script type="application/ld+json">')[1].split("</script>")[0])
